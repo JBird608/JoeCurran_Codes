fa9005fd4d89602dd50226c58fdad6fd47d9b60f
Joe Curran <jbird608@gmail.com>
2015-09-12 16:27:35 +0100

    Added foundation assets and some HTML design elements.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('core_title')</title>
 
-</head>
-<body>
+    <!-- Core Website Styles -->
+    <link rel="stylesheet" href="css/core_eagle.css" />
+    @yield('bolt_css')
 
-<h1>Hello this is the demo page!</h1>
+</head>
+<body id="top">
 
 @yield('content')
+
+    <div id="core_follow">
+        <div class="row">
+            <div class="large-12 columns">
+                <p>&copy; 2015 <a href="#">Joe Curran</a>. Made in the UK.</p>
+                <p>Well, that's it. Why not check me out else where?</p>
+                <ul class="social">
+                    <li class="social-twitter"><a href="#" alt="">Twitter</a></li>
+                    <li class="social-pinterest"><a href="#" alt="">Pinterest</a></li>
+                    <li class="social-linkedin"><a href="#" alt="">LinkedIn</a></li>
+                    <li class="social-behance"><a href="#" alt="">Behance</a></li>
+                    <li class="social-skype"><a href="#" alt="">Skype</a></li>
+                    <li class="social-email"><a href="#" alt="">E-Mail</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div id="core_end">
+        <div class="row">
+            <div class="large-12 columns">
+                <p>// That's it, you've hit the bottom. <a href="#top" class="magic">Fast Travel Magic!</a> //</p>
+            </div>
+        </div>
+    </div>
 
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -2,11 +2,17 @@
 
 @section('core_title')
     Joe Curran Codes :: I Love Coding! 
-@stop
+@endsection
+
+@section('bolt_css')
+
+@endsection
 
 @section('content')
     <h2>What the hell is going on here?</h2>
     <p>Don't be silly there pal! Its only a simple page, this is nice!</p>
-    
+
+
+
     <p>What about this!</p>
-@stop
+@endsection
