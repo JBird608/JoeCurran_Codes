d34b5d08a7e6be6e9cd888c0645ba139644c95d9
Joe Curran <jbird608@gmail.com>
2015-09-30 00:18:14 +0100

    Routes reorders and formated

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,55 +1,42 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+/**
+ * Requesting the Homepage.
+ */
+    Route::get('/', function () {
+        return view('core.standard');
+    });
 
-// Regesting Home Page //
-Route::get('/', function () {
-    return view('core.standard');
-});
-
-// Regestering Static Pages //
+/**
+ * Static or General Page Requests.
+ */
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 Route::get('home', 'PagesController@home');
 
-// Regestering Blog Requests //
-//Route::get('blog', 'BlogController@index');
-
-//// BLOG REDIRECTS //
-//Route::get('article', 'BlogController@redirect');
-//Route::get('articles', 'BlogController@redirect');
-//
-//Route::get('article/create', 'BlogController@create');
-//Route::post('article/store', 'BlogController@store');
-//Route::get('article/{slug}', 'BlogController@article');
-
 /**
- * Creating a resource controller for the Articles/Blog.
+ * Blog or Article Requests.
  */
 Route::resource('blog', 'BlogController');
 
+/**
+ * User Authentication, Registation & Profiles Requests.
+ */
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('logout', 'Auth\AuthController@getLogout');
+Route::get('register', 'Auth\AuthController@getRegister');
+Route::post('register', 'Auth\AuthController@postRegister');
+
+/**
+ * Testing & Temporary Requests
+ */
 Route::get('user', function () {
     return Auth::user();
 });
 
 
-// Authentication routes...
-Route::get('login', 'Auth\AuthController@getLogin');
-Route::post('login', 'Auth\AuthController@postLogin');
-Route::get('logout', 'Auth\AuthController@getLogout');
 
-// Registration routes...
-Route::get('register', 'Auth\AuthController@getRegister');
-Route::post('register', 'Auth\AuthController@postRegister');
 
 
 
