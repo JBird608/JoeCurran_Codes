5d737b7ed18c78d8f0591198f42ebc25142d76c9
Joe Curran <jbird608@gmail.com>
2015-11-09 22:31:54 +0000

    BugFix: Insert for Articles Table

--- database/migrations/2015_09_22_154044_create_articles_table.php
+++ database/migrations/2015_09_22_154044_create_articles_table.php
@@ -34,7 +34,7 @@ class CreateArticlesTable extends Migration
             $table->timestamp('published');
             $table->timestamps();
         });
-        DB::table('metas')->insert(
+        DB::table('articles')->insert(
             array(
                 array(1, '16BsO3jwqHCWtqUu', 1, 'Motion UI (Foundation 6)', '/img/uploads/2015/Nov/09/foundation_6_motion_ui_cover.png', '/img/uploads/2015/Nov/09/foundation_6_motion_ui_sml.png', 'With the new release of Foundation 6 around the corner I take a look at one of is new features, Motion UI. A SASS library for quickly creating CSS transition and animations.', '##Motion UI \r\n\r\nBody written in Markdown. \r\n', 'foundation_6_motion_ui', 2, 3, 0, 0, 'CSS, SCSS, UI, Foundation, Motion UI, ZURB', NULL, NULL, '2015-11-09 21:43:51', '2015-11-09 21:43:51', '2015-11-09 21:43:51'),
                 array(2, 'e66cVEJFY1qKn0ZM', 1, 'What makes great SEO?', '/img/uploads/2015/Nov/09/what_makes_great_seo_cover.png', '/img/uploads/2015/Nov/09/what_makes_great_seo_sml.png', 'There are many things that people claim will improve your websites SEO, but how many are true? What can you do to improve your sites SEO today? ', 'Body Goes Here Using MarkDown. ', 'what_makes_great_seo', 2, 8, 0, 0, 'SEO', NULL, NULL, '2015-11-02 21:51:28', '2015-11-09 21:51:28', '2015-11-09 21:51:28'),
