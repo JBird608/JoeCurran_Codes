fad37729bc7f28f667555e3f88951c21ba38cba7
Joe Curran <jbird608@gmail.com>
2015-11-09 21:28:34 +0000

    Changed Meta Insert Statment

--- database/migrations/2015_10_13_104629_create_metas_table.php
+++ database/migrations/2015_10_13_104629_create_metas_table.php
@@ -24,18 +24,18 @@ class CreateMetasTable extends Migration
             $table->string('og_type')->default('website');
             $table->timestamps();
         });
-        DB::table('users')->insert(
+        DB::table('metas')->insert(
             array(
-                array('code' => '2vq4KHidVogKpgZ8', 'title' => 'Article Creation', 'discription' => ''),
-                array('code' => '2vq4KHidVogKpgZe', 'title' => 'Blog', 'discription' => ''),
-                array('code' => 'DGgsmT9iTEObJ1dD', 'title' => 'Web Designer & Developer :: Joe Curran Codes', 'discription' => ''),
-                array('code' => 'tUZ8MMVRz6yb88iv', 'title' => 'About Joe Curran :: Joe Curran Codes', 'discription' => ''),
-                array('code' => 'rJvGPUVfUxm2TBDg', 'title' => 'Get in Touch :: Joe Curran Codes', 'discription' => ''),
-                array('code' => 'zbQTVfF6VfJry6LI', 'title' => 'My Projects :: Joe Curran Codes', 'discription' => ''),
-                array('code' => '1o3wTvPBraUILjQZ', 'title' => 'Dashboard :: Joe Curran Codes', 'discription' => ''),
-                array('code' => '5meuVGy2zOKvjsZO', 'title' => 'Once in a Blue Moon :: Joe Curran Codes', 'discription' => ''),
-                array('code' => 'jeSu7UYVbVM6S0IF', 'title' => 'Create an Article :: Joe Curran Codes', 'discription' => ''),
-                array('code' => '8PWLY8FGtvp2nQ5D', 'title' => 'Article Categories', 'discription' => ''),
+                array( 'code' => '2vq4KHidVogKpgZ8', 'title' => 'Article Creation', 'discription' => ''),
+                array( 'code' => '2vq4KHidVogKpgZe', 'title' => 'Blog', 'discription' => ''),
+                array( 'code' => 'DGgsmT9iTEObJ1dD', 'title' => 'Web Designer & Developer :: Joe Curran Codes', 'discription' => ''),
+                array( 'code' => 'tUZ8MMVRz6yb88iv', 'title' => 'About Joe Curran :: Joe Curran Codes', 'discription' => ''),
+                array( 'code' => 'rJvGPUVfUxm2TBDg', 'title' => 'Get in Touch :: Joe Curran Codes', 'discription' => ''),
+                array( 'code' => 'zbQTVfF6VfJry6LI', 'title' => 'My Projects :: Joe Curran Codes', 'discription' => ''),
+                array( 'code' => '1o3wTvPBraUILjQZ', 'title' => 'Dashboard :: Joe Curran Codes', 'discription' => ''),
+                array( 'code' => '5meuVGy2zOKvjsZO', 'title' => 'Once in a Blue Moon :: Joe Curran Codes', 'discription' => ''),
+                array( 'code' => 'jeSu7UYVbVM6S0IF', 'title' => 'Create an Article :: Joe Curran Codes', 'discription' => ''),
+                array( 'code' => '8PWLY8FGtvp2nQ5D', 'title' => 'Article Categories', 'discription' => ''),
             )
         );
     }
